a1c2bf4f8673fc07e48af296e4d71f649bef339c
Natalia Vardazaryan <88931980+VardazaryanN@users.noreply.github.com>
2022-02-07 15:00:34 +0400

    Add files via upload

---
+++ 1/script.ts
@@ -0,0 +1,12 @@
+function test(num: number): string || number{
+    if (num % 2 === 0){
+        return "candy";
+    } else if { (num % 11 === 0){
+        return "bar";
+    } else if(num % 22 == 0){
+        return "candybar";
+    } else {
+		return num;
+	}
+
+}
